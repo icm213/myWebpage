adeffd96248aee648a0562afedbb1843b9dd26c0
icm213 <marekpownuk@gmail.com>
2022-03-27 21:12:06 +0200

    first commit

---
+++ js/subsite3Script.js
@@ -0,0 +1,32 @@
+function darkMode() {
+    var element = document.getElementById("myContainer");
+    element.classList.toggle("dark-mode");
+}
+function darkButton() {
+    var element1 = document.getElementById("myButton");
+    element1.classList.toggle("dark-button");
+}
+function darkNavbar() {
+    var element2 = document.getElementById("myNavbar");
+    element2.classList.toggle("dark-navbar");
+}
+function darkHolder() {
+    var element3 = document.getElementById("myHolder");
+    element3.classList.toggle("dark-holder");
+}
+function darkFont() {
+    var element4 = document.getElementById("myDarkFont");
+    element4.classList.toggle("dark-font");
+}
+function darkFont1() {
+    var element4 = document.getElementById("myDarkFont1");
+    element4.classList.toggle("dark-font");
+}
+function myNav() {
+    var x = document.getElementById('myNav');
+    x.classList.toggle('show-navbar')
+}
+function darkFooter() {
+    var x = document.getElementById('myFooter');
+    x.classList.toggle('dark-footer')
+}
